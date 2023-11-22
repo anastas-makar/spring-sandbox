8b867d8f4e6567b559d1bbe00095363f0e98b184
Anastasiia Makarenko <irishmoth@yandex.ru>
2023-11-22 18:03:29 +0300

    Добавляю проект не как сабмодуль

---
+++ hello-restful/src/main/java/pro/progr/hellorestful/HelloRestfulApplication.java
@@ -0,0 +1,13 @@
+package pro.progr.hellorestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloRestfulApplication.class, args);
+    }
+
+}
---
+++ hello-restful/src/test/java/pro/progr/hellorestful/HelloRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.progr.hellorestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
