d9e3c7f8e6513d272a0593d432236d45b5e4402e
Anastasiia Makarenko <irishmoth@yandex.ru>
2023-11-23 11:50:03 +0300

    consuming-restful

---
+++ consuming-restful/src/main/java/pro/progr/consumingrestful/ConsumingRestfulApplication.java
@@ -0,0 +1,13 @@
+package pro.progr.consumingrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestfulApplication.class, args);
+    }
+
+}
