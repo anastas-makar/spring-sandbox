2934fd07b3ed50510eee82e2632defdced6cd7cd
Anastasiia Makarenko <irishmoth@yandex.ru>
2023-11-27 12:11:26 +0300

    Файлы как-то загружаются (первый загрузился с ошибкой)

---
+++ uploading-files/src/main/java/pro/progr/uploadingfiles/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package pro.progr.uploadingfiles;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
