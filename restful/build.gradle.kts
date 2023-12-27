76da6f60b643545e88066a548dd1cb5e04170c94
Anastasiia Makarenko <irishmoth@yandex.ru>
2023-12-27 17:41:47 +0300

    Начинаю добавлять flyway в проект resful

--- restful/build.gradle.kts
+++ restful/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("org.flywaydb.flyway") version "10.4.1"
 }
 
 group = "pro.progr"
@@ -18,6 +19,13 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+flyway {
+    url = System.getenv("DB_URL")
+    user = System.getenv("DB_USER")
+    password = System.getenv("DB_PASSWORD")
 }
 
 tasks.withType<Test> {
