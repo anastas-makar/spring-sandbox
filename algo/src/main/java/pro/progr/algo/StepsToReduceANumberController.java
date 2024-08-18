8572e7df3129da2bf087bbd93a8f596cfb76b267
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-08-18 03:04:32 +0300

    Задача: количество шагов, чтобы уменьшить число до нуля

---
+++ algo/src/main/java/pro/progr/algo/StepsToReduceANumberController.java
@@ -0,0 +1,41 @@
+package pro.progr.algo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/steps_to_reduce_number")
+public class StepsToReduceANumberController implements AlgoProblemInterface {
+
+    @Override
+    @GetMapping
+    public String description() {
+        return "Дано целое число num. " +
+                "Верните количество шагов, чтобы уменьшить его до нуля.\n" +
+                "\n" +
+                "За один шаг, если текущее число четное, вы должны разделить его на 2, " +
+                "в противном случае вы должны вычесть из него 1. " +
+                "(https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/)";
+    }
+
+    @GetMapping(params = {"num"})
+    public String solution(@RequestParam String num) {
+        var number = Integer.parseInt(num);
+
+        var stepCount = 0;
+
+        while (number > 0) {
+            if (number % 2 > 0) {
+                number -= 1;
+            } else {
+                number /= 2;
+            }
+
+            stepCount++;
+        }
+
+        return String.valueOf(stepCount);
+    }
+}
